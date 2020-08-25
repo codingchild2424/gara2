b3efd2d38a95b16c6610a5b4d569e57a0e2a4649
Life4honor <choejy0329@gmail.com>
2020-08-25 20:26:09 +0900

    Update app.py

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -54,3 +54,76 @@ def get_groups():
 def get_picture_url(name, class_code):
     picture_url = None
     return picture_url  # 등록한 학생 사진 URL
+
+
+class Student(db.Model):
+
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    class_code = db.Column(db.Integer)
+    personality_id = db.Column(db.Integer, db.ForeignKey("personalities.id"))
+
+    def __init__(self, name, class_code, personality_id=None):
+        self.name = name
+        self.class_code = class_code
+        self.personality_id = personality_id
+
+
+class Problem(db.Model):
+
+    __tablename__ = "problems"
+
+    id = db.Column(db.Integer, primary_key=True)
+    context = db.Column(db.Text)
+    options = db.Column(db.Text)
+
+    def __init__(self, context, options):
+        self.context = context
+        self.options = options
+
+
+class Personality(db.Model):
+
+    __tablename__ = "personalities"
+
+    id = db.Column(db.Integer, primary_key=True)
+    group = db.Column(db.Text)
+    description = db.Column(db.Text)
+    linked_personality_jobs = db.relationship(
+        "PersonalityJob", backref="personalities", lazy="dynamic"
+    )
+    linked_students = db.relationship(
+        "Student", backref="personalities", lazy="dynamic"
+    )
+
+    def __init__(self, group, description):
+        self.group = group
+        self.description = description
+
+
+class Job(db.Model):
+
+    __tablename__ = "jobs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text)
+    linked_personality_jobs = db.relationship(
+        "PersonalityJob", backref="job", lazy="dynamic"
+    )
+
+    def __init__(self, description):
+        self.description = description
+
+
+class PersonalityJob(db.Model):
+    __tablename__ = "personalityjob"
+
+    id = db.Column(db.Integer, primary_key=True)
+    personality_id = db.Column(db.Integer, db.ForeignKey("personalities.id"))
+    job_id = db.Column(db.Integer, db.ForeignKey("jobs.id"))
+
+    def __init__(self, personality_id, job_id):
+        self.personality_id = personality_id
+        self.job_id = job_id
