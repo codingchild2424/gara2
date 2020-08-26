26c9ed75173fd031fb3de931d3a2beb9e4e6719f
juhong7586 <juhong0422@gmail.com>
2020-08-26 16:44:57 +0900

    Add files via upload

---
+++ frontend/script3.js
@@ -0,0 +1,37 @@
+﻿
+var totalScore;
+var percent = 0;
+var nextButton = document.getElementById('nextButton');
+var regionScore = 0;
+
+function add() {
+    regionScore += 1;
+}
+
+function minus() {
+    regionScore = regionScore - 1;
+}
+
+nextButton.addEventListener('click', function () {
+    localStorage.setItem("regionScore", regionScore.value);
+    console.log(regionScore);
+
+    $('#progressBar').css('width', '0px');
+    $('#progressBar').addClass('progress-bar-striped active');
+
+
+    
+    percent += 20;
+    $('#progressBar').css('width', percent + '%');
+    $('#progressBar').html(percent + '%');
+
+
+    if (percent >= 100) {
+        $('#progressBar').removeClass('progress-bar-striped active');
+        $('#nextButton').attr('disabled', true);
+        $('#nextStage').css('display', 'inline');
+
+    }
+});
+
+
