01634f883511aa2cb696402139dea04f1387db7f
Life4honor <choejy0329@gmail.com>
2020-08-27 19:28:11 +0900

    Update Dockerfile ENTRYPOINT

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-from . import dbclient, minioclient
-import json
+import dbclient, minioclient, json
 
 app = Flask(__name__)
 CORS(app)
--- backend/main.py
+++
@@ -1,5 +0,0 @@
-from flaskr.app import app
-
-
-if __name__ == "__main__":
-    app.run()
